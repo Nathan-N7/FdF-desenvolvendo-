1eb7d27def03da6873f478618766956e9b03b60e
Nathan-N7 <nathan.rod1710@gmail.com>
2024-11-24 21:46:42 -0300

    fdf

--- matriz.c
+++ matriz.c
@@ -1,15 +1,17 @@
 #include "gnl/get_next_line.h"
+#include "libft/libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 typedef struct s_matriz
 {
-	void	*x;
-	void	*y;
-	void	**z;
+	int	x;
+	int	y;
+	int	**mapa;
 }	t_matriz;
 
-int	count_x(char *str)
+int	count_x(char	*str)
 {
 	int	i;
 	int	count;
@@ -20,7 +22,7 @@ int	count_x(char *str)
 	{
 		if (str[i] != ' ')
 			count++;
-		while (str[i] != ' ')
+		while (str[i] != ' ' && str[i] != '\0')
 			i++;
 		i++;
 	}
@@ -35,25 +37,89 @@ int	count_y(int	fd)
 	line = get_next_line(fd);
 	while (line != NULL)
 	{	
+		free(line);
 		line = get_next_line(fd);
 		i++;
 	}
-	return (i - 1);
+	close(fd);
+	return (i);
 }
 
-t_matriz	**matriz(int fd)
+int	*mapa_line(char	*line, int x)
 {
-	int	x;
-	int	y;
-
-	x = count_x(fd);
-	y = count_y(fd);
-
+	char	**num;
+	int	*linem;
+	int	i;
 	
+	i = 0;
+	linem = malloc(sizeof(int) * x);
+	num = ft_split(line, ' ');
+	if (num == NULL || linem == NULL)
+		return (NULL);
+	while  (num[i] != NULL)
+	{
+		linem[i] = ft_atoi(num[i]);
+		free(num[i]);
+		i++;
+	}
+	free(num[i]);
+	return (linem);
+}
+
+void	creat_matriz(t_matriz *ptr, int fd)
+{
+	char	*line;
+	int	i;
+	
+	line = get_next_line(fd);
+	ptr->mapa = (int **)malloc(sizeof(int *) * ptr->y);
+	if (ptr->mapa == NULL)
+		return ;	
+	i = 0;
+	while (line != NULL)
+	{
+		ptr->mapa[i] = mapa_line(line, ptr->x);
+		if (ptr->mapa[i] == NULL)
+		{
+			free(line);
+			return ;
+		}
+		free(line);
+		line = get_next_line(fd);
+		i++;
+	}
 }
 
 int	main()
 {
-	int	fd = open("test.txt", O_RDONLY);
-	printf("%d", count_y(fd));
+	int	fd;
+	char	*line;
+	int	i;
+	int	k;
+	t_matriz	*ptr;
+
+	i = 0;
+	k = 0;
+	ptr  = malloc(sizeof(t_matriz));
+	fd = open("42.fdf", O_RDONLY);
+	if (fd < 0 || ptr == NULL)
+		return (-1);
+
+	line = get_next_line(fd);
+	ptr->x = count_x(line);
+	ptr->y = count_y(fd);
+	fd = open("42.fdf", O_RDONLY);
+	creat_matriz(ptr, fd);
+	
+	while (i < ptr->y)
+	{
+		while (k < ptr->x)
+		{
+			printf("%d ", ptr->mapa[i][k]);
+			k++;
+		}
+		printf("\n");
+		k = 0;
+		i++;
+	}
 }
